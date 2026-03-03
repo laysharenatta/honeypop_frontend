f42d18d4cd95168828e9a27bd40caa39c15ff673
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2026-03-02 19:17:52 -0600

    feat: (ProductosMovimientosView) :sparkles: Create the view for the product movements

---
+++ src/views/ProductosMovimientosView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-center py-8">Movimientos del producto {{ route.params.id }}</h1>
+    <p class="text-center text-gray-600 mb-8">Movimientos de inventario del producto.</p>
+    <ProductosMovimientosTable />
+  </div>
+
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+import ProductosMovimientosTable from '@/components/movements/ProductosMovimientosTable.vue';
+</script>
