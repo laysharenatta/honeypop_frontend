e21c0a1b4cf1ba498187de536615d67da3252f09
ArmandoVallejo <juan.vallejo@cceo.com.mx>
2026-03-02 18:15:31 -0600

    feat: (Auth) :sparkles: Add the functions of the auth feature

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
 
-<Navbar />
+<Navbar v-if="isAuthenticated" />
 <RouterView />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Navbar  from "@/components/widgets/Navbar.vue";
 import { RouterView } from 'vue-router'
+
+const isAuthenticated = ref(localStorage.getItem('token'));
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import HomeView from '@/views/HomeView.vue'
-import ProveedoresView from '@/views/ProveedoresView.vue'
-import ProductosView from '@/views/ProductosView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue';
+import ProveedoresView from '@/views/ProveedoresView.vue';
+import ProductosView from '@/views/ProductosView.vue';
+import LoginView from '@/views/LoginView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,18 +11,44 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: '/proveedores',
       name: 'proveedores',
       component: ProveedoresView,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/productos',
       name: 'productos',
       component: ProductosView,
+      meta: {
+        requiresAuth: true
+      }
     }
   ],
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token');
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
+});
+
+export default router;
